540281957277efa2e45d78d566c00fa2c03fd707
Francesco Biscani <bluescarni@gmail.com>
2019-04-01 17:05:16 +0200

    Some more work on bit packing.

--- include/piranha/detail/bit_packing.hpp
+++ include/piranha/detail/bit_packing.hpp
@@ -56,16 +56,34 @@ public:
     {
         constexpr auto nbits = static_cast<unsigned>(limits_digits<value_type>);
         if (piranha_unlikely(size > nbits)) {
-            piranha_throw(::std::overflow_error, "");
+            piranha_throw(::std::overflow_error, "The number of values to be pushed to this bit packer ("
+                                                     + detail::to_string(size) + ") is larger than the bit width ("
+                                                     + detail::to_string(nbits) + ") of the value type of the packer");
         }
         if (size) {
+            // NOTE: compute these values only if we are actually going
+            // to pack values (if size is zero, no packing ever happens).
+            //
+            // m_pbits is the number of bits that can be used by each
+            // packed value.
             m_pbits = nbits / size;
+            // m_max is the maximum packed value (in unsigned format).
+            // It is a sequence of m_pbits 1 bits.
             m_max = static_cast<value_type>(-1) >> (nbits - m_pbits);
             if constexpr (is_signed_v<T>) {
+                // NOTE: the signed offset is needed only if T is a signed type.
+                //
+                // This is the offset to be applied to the packed values
+                // when T is signed. If we have m_pbits available for each
+                // packed value, then the allowed range for signed packed values
+                // is [-2**(m_pbits-1), 2**(m_pbits-1) - 1], same as two's
+                // complement. We add 2**(m_pbits-1) to the signed packed values,
+                // so that we obtain an unsigned range of [0, 2**m_pbits - 1].
                 m_s_offset = value_type(1) << (m_pbits - 1u);
             }
         }
     }
+    // Add the next value to be encoded.
     bit_packer &operator<<(const T &n)
     {
         if (piranha_unlikely(m_index == m_size)) {
@@ -82,12 +100,26 @@ public:
             }
         }();
         if (piranha_unlikely(shift_n > m_max)) {
-            piranha_throw(::std::overflow_error, "");
+            piranha_throw(::std::overflow_error,
+                          "The value being pushed to this bit packer (" + detail::to_string(shift_n)
+                              + ") is larger than the maximum allowed value (" + detail::to_string(m_max) + ")");
         }
         m_value += shift_n << m_cur_shift;
         ++m_index;
         m_cur_shift += m_pbits;
         return *this;
+    }
+    // Get the encoded value.
+    value_type get() const
+    {
+        if (piranha_unlikely(m_index < m_size)) {
+            piranha_throw(::std::out_of_range, "Cannot fetch the packed value from this bit packer: the number of "
+                                               "values pushed to the packer ("
+                                                   + detail::to_string(m_index)
+                                                   + ") is less than the size used for construction ("
+                                                   + detail::to_string(m_size) + ")");
+        }
+        return m_value;
     }
 
 private:
