edf9ffe342735bc1b0f56f2d9fc43aede6918afa
Francesco Biscani <bluescarni@gmail.com>
2019-04-03 10:25:21 +0200

    More testing bits.

--- test/utils_bit_packing.cpp
+++ test/utils_bit_packing.cpp
@@ -15,8 +15,10 @@
 #include <stdexcept>
 #include <tuple>
 #include <type_traits>
+#include <vector>
 
 #include <piranha/config.hpp>
+#include <piranha/detail/to_string.hpp>
 #include <piranha/detail/tuple_for_each.hpp>
 #include <piranha/type_traits.hpp>
 
@@ -44,6 +46,7 @@ TEST_CASE("bit_packer_unpacker")
         using Catch::Matchers::Contains;
 
         const auto [lim_min, lim_max] = detail::limits_minmax<int_t>;
+        constexpr auto nbits = static_cast<unsigned>(detail::limits_digits<value_t>);
 
         // Start with an empty packer.
         bp_t bp0(0);
@@ -99,5 +102,69 @@ TEST_CASE("bit_packer_unpacker")
 #if defined(PIRANHA_HAVE_GCC_INT128)
         }
 #endif
+
+        // Random testing with variable sizes.
+#if defined(PIRANHA_HAVE_GCC_INT128)
+        if constexpr (!std::is_same_v<int_t, __int128_t> && !std::is_same_v<int_t, __uint128_t>) {
+#endif
+            for (auto i = 2u; i < nbits; ++i) {
+                const auto [cur_min, cur_max] = [pbits = nbits / i]() {
+                    if constexpr (is_signed_v<int_t>) {
+                        return std::tuple{-(int_t(1) << (pbits - 1u)), (int_t(1) << (pbits - 1u)) - int_t(1)};
+                    } else {
+                        return std::tuple{int_t(0), (int_t(1) << pbits) - int_t(1)};
+                    }
+                }();
+                std::uniform_int_distribution<int_t> idist(cur_min, cur_max);
+                std::vector<int_t> v(i);
+                for (auto k = 0; k < ntrials; ++k) {
+                    bp1 = bp_t(i);
+                    for (auto &n : v) {
+                        n = idist(rng);
+                        bp1 << n;
+                    }
+                    bu1 = bu_t(bp1.get(), i);
+                    for (const auto &n : v) {
+                        bu1 >> out;
+                        REQUIRE(out == n);
+                    }
+                }
+
+                // Check out of range packing.
+                bp1 = bp_t(i);
+                if constexpr (is_signed_v<int_t>) {
+                    REQUIRE_THROWS_WITH(bp1 << (cur_min - int_t(1)),
+                                        Contains("The signed value being pushed to this bit packer ("
+                                                 + detail::to_string(cur_min - int_t(1))
+                                                 + ") is outside the allowed range [" + detail::to_string(cur_min)
+                                                 + ", " + detail::to_string(cur_max) + "]"));
+                    REQUIRE_THROWS_AS(bp1 << (cur_min - int_t(1)), std::overflow_error);
+                    REQUIRE_THROWS_WITH(bp1 << (cur_max + int_t(1)),
+                                        Contains("The signed value being pushed to this bit packer ("
+                                                 + detail::to_string(cur_max + int_t(1))
+                                                 + ") is outside the allowed range [" + detail::to_string(cur_min)
+                                                 + ", " + detail::to_string(cur_max) + "]"));
+                    REQUIRE_THROWS_AS(bp1 << (cur_max + int_t(1)), std::overflow_error);
+                } else {
+                }
+            }
+#if defined(PIRANHA_HAVE_GCC_INT128)
+        }
+#endif
+
+        // Error checking.
+        REQUIRE_THROWS_WITH(bp_t(nbits + 1u),
+                            Contains("The number of values to be pushed to this bit packer ("
+                                     + detail::to_string(nbits + 1u) + ") is larger than the bit width ("
+                                     + detail::to_string(nbits) + ") of the value type of the packer"));
+        REQUIRE_THROWS_AS(bp_t(nbits + 1u), std::overflow_error);
+
+        bp1 = bp_t(3);
+        bp1 << int_t(0) << int_t(0) << int_t(0);
+        REQUIRE_THROWS_WITH(
+            bp1 << int_t(0),
+            Contains("Cannot push any more values to this bit packer: the number of "
+                     "values already pushed to the packer is equal to the size used for construction (3)"));
+        REQUIRE_THROWS_AS(bp1 << int_t(0), std::out_of_range);
     });
 }
