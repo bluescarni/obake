3909f721c6c6ef0e4425ce67c71e1f695b596a9b
Francesco Biscani <bluescarni@gmail.com>
2019-04-03 16:55:17 +0200

    Try adding clang builds. [skip appveyor]

--- tools/circleci_bionic.sh
+++ tools/circleci_bionic.sh
@@ -7,7 +7,7 @@ set -x
 set -e
 
 # Core deps.
-sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev wget
+sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev wget clang libc++-dev
 
 # Create the build dir and cd into it.
 mkdir build
@@ -16,10 +16,18 @@ cd build
 # Download and install mppp.
 sh ../tools/circleci_install_mppp.sh
 
-# Configure and build piranha.
+# GCC build.
 cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=~/.local -DPIRANHA_BUILD_TESTS=YES -DCMAKE_CXX_FLAGS="-fsanitize=undefined"
 make -j2 VERBOSE=1
+# Run the tests.
+ctest -V
 
+# clang build.
+cd ..
+mkdir build_clang
+cd build_clang
+CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=~/.local -DPIRANHA_BUILD_TESTS=YES
+make -j2 VERBOSE=1
 # Run the tests.
 ctest -V
 
