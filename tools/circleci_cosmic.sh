d82f2a7a3e931e138f5a22ef2eab8d6c1fd3a53a
Francesco Biscani <bluescarni@gmail.com>
2019-04-03 16:13:52 +0200

    WIP [skip appveyor]

---
+++ tools/circleci_cosmic.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev wget
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+# Download and install mppp.
+MPPP_VERSION="0.13"
+wget https://github.com/bluescarni/mppp/archive/v${MPPP_VERSION}.tar.gz -O mppp.tar.gz
+tar xzf mppp.tar.gz
+cd mppp-${MPPP_VERSION}
+mkdir build
+cd build
+cmake ../ -DMPPP_WITH_MPFR=YES -DMPPP_WITH_QUADMATH=YES -DCMAKE_INSTALL_PREFIX=~/.local
+make install
+cd ..
+cd ..
+
+
+
+set +e
+set +x
