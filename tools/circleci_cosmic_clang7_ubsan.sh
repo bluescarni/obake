e62cfaed34e5efc1a45744fe7c469956aff508f6
Francesco Biscani <bluescarni@gmail.com>
2019-05-10 13:47:33 +0200

    Some tentative fixes. [skip appveyor]

--- tools/circleci_cosmic_clang7_ubsan.sh
+++ tools/circleci_cosmic_clang7_ubsan.sh
@@ -13,12 +13,15 @@ sudo apt-get install build-essential cmake libgmp-dev libmpfr-dev wget clang lib
 mkdir build
 cd build
 
+export CC=clang
+export CXX=clang++
+
 # Download and install mppp and abseil.
 bash ../tools/circleci_install_mppp.sh
 bash ../tools/circleci_install_abseil.sh
 
 # clang build.
-CC=clang CXX=clang++ cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=~/.local -DPIRANHA_BUILD_TESTS=YES -DCMAKE_CXX_FLAGS="-fsanitize=undefined" -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/8/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/8/libquadmath.so
+cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=~/.local -DPIRANHA_BUILD_TESTS=YES -DCMAKE_CXX_FLAGS="-fsanitize=undefined" -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/8/include/ -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/8/libquadmath.so
 make -j2 VERBOSE=1
 # Run the tests.
 ctest -V
