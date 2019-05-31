003e38601df70595f091b1ebfe96f6266bfd1fe5
Francesco Biscani <bluescarni@gmail.com>
2019-05-31 11:10:38 +0200

    Add the key_merge_symbols() function.

---
+++ include/piranha/key/key_merge_symbols.hpp
@@ -0,0 +1,99 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the piranha library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef PIRANHA_KEY_KEY_MERGE_SYMBOLS_HPP
+#define PIRANHA_KEY_KEY_MERGE_SYMBOLS_HPP
+
+#include <type_traits>
+#include <utility>
+
+#include <piranha/config.hpp>
+#include <piranha/detail/not_implemented.hpp>
+#include <piranha/detail/priority_tag.hpp>
+#include <piranha/detail/ss_func_forward.hpp>
+#include <piranha/symbols.hpp>
+#include <piranha/type_traits.hpp>
+
+namespace piranha
+{
+
+namespace customisation
+{
+
+// External customisation point for piranha::key_merge_symbols().
+template <typename T
+#if !defined(PIRANHA_HAVE_CONCEPTS)
+          ,
+          typename = void
+#endif
+          >
+inline constexpr auto key_merge_symbols = not_implemented;
+
+} // namespace customisation
+
+namespace detail
+{
+
+// Highest priority: explicit user override in the external customisation namespace.
+template <typename T>
+constexpr auto key_merge_symbols_impl(T &&x, const symbol_idx_map<symbol_set> &ins_map, const symbol_set &ss,
+                                      priority_tag<1>)
+    PIRANHA_SS_FORWARD_FUNCTION((customisation::key_merge_symbols<T &&>)(::std::forward<T>(x), ins_map, ss));
+
+// Unqualified function call implementation.
+template <typename T>
+constexpr auto key_merge_symbols_impl(T &&x, const symbol_idx_map<symbol_set> &ins_map, const symbol_set &ss,
+                                      priority_tag<0>)
+    PIRANHA_SS_FORWARD_FUNCTION(key_merge_symbols(::std::forward<T>(x), ins_map, ss));
+
+template <typename T>
+using key_merge_symbols_impl_ret_t
+    = decltype(detail::key_merge_symbols_impl(::std::declval<T>(), ::std::declval<const symbol_idx_map<symbol_set> &>(),
+                                              ::std::declval<const symbol_set &>(), priority_tag<1>{}));
+
+template <typename T,
+          ::std::enable_if_t<::std::is_same_v<remove_cvref_t<T>, detected_t<key_merge_symbols_impl_ret_t, T>>, int> = 0>
+constexpr auto key_merge_symbols_impl_with_ret_check(T &&x, const symbol_idx_map<symbol_set> &ins_map,
+                                                     const symbol_set &ss)
+    PIRANHA_SS_FORWARD_FUNCTION(detail::key_merge_symbols_impl(::std::forward<T>(x), ins_map, ss, priority_tag<1>{}));
+
+} // namespace detail
+
+inline constexpr auto key_merge_symbols =
+    [](auto &&x, const symbol_idx_map<symbol_set> &ins_map, const symbol_set &ss) PIRANHA_SS_FORWARD_LAMBDA(
+        detail::key_merge_symbols_impl_with_ret_check(::std::forward<decltype(x)>(x), ins_map, ss));
+
+namespace detail
+{
+
+template <typename T>
+using key_merge_symbols_t = decltype(::piranha::key_merge_symbols(
+    ::std::declval<T>(), ::std::declval<const symbol_idx_map<symbol_set> &>(), ::std::declval<const symbol_set &>()));
+
+}
+
+template <typename T>
+using is_symbols_mergeable_key = is_detected<detail::key_merge_symbols_t, T>;
+
+template <typename T>
+inline constexpr bool is_symbols_mergeable_key_v = is_symbols_mergeable_key<T>::value;
+
+#if defined(PIRANHA_HAVE_CONCEPTS)
+
+template <typename T>
+PIRANHA_CONCEPT_DECL SymbolsMergeableKey
+    = requires(T &&x, const symbol_idx_map<symbol_set> &ins_map, const symbol_set &ss)
+{
+    ::piranha::key_merge_symbols(::std::forward<T>(x), ins_map, ss);
+};
+
+#endif
+
+} // namespace piranha
+
+#endif
---
+++ test/key_key_merge_symbols.cpp
@@ -0,0 +1,157 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the piranha library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <piranha/key/key_merge_symbols.hpp>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <type_traits>
+
+#include <piranha/config.hpp>
+#include <piranha/symbols.hpp>
+#include <piranha/type_traits.hpp>
+
+using namespace piranha;
+
+struct no_kms00 {
+};
+
+// ADL-based implementations.
+namespace ns
+{
+
+struct kms00 {
+};
+
+kms00 key_merge_symbols(const kms00 &, const symbol_idx_map<symbol_set> &, const symbol_set &);
+
+struct no_kms00 {
+};
+
+// Wrong return type.
+void key_merge_symbols(const no_kms00 &, const symbol_idx_map<symbol_set> &, const symbol_set &);
+
+struct kms01 {
+};
+
+// Only some overloads.
+kms01 key_merge_symbols(kms01 &, const symbol_idx_map<symbol_set> &, const symbol_set &);
+
+} // namespace ns
+
+// External customisation point.
+struct ext00 {
+};
+
+struct ext01 {
+};
+
+struct noext00 {
+};
+
+namespace piranha::customisation
+{
+
+template <typename T>
+#if defined(PIRANHA_HAVE_CONCEPTS)
+requires SameCvr<T, ext00> inline constexpr auto key_merge_symbols<T>
+#else
+inline constexpr auto key_merge_symbols<T, std::enable_if_t<is_same_cvr_v<T, ext00>>>
+#endif
+    = [](auto &&, const symbol_idx_map<symbol_set> &, const symbol_set &) constexpr noexcept
+{
+    return ext00{};
+};
+
+template <typename T>
+#if defined(PIRANHA_HAVE_CONCEPTS)
+requires SameCvr<T, ext01> inline constexpr auto key_merge_symbols<T>
+#else
+inline constexpr auto key_merge_symbols<T, std::enable_if_t<is_same_cvr_v<T, ext01>>>
+#endif
+    = [](auto &, const symbol_idx_map<symbol_set> &, const symbol_set &) constexpr noexcept
+{
+    return ext01{};
+};
+
+template <typename T>
+#if defined(PIRANHA_HAVE_CONCEPTS)
+requires SameCvr<T, noext00> inline constexpr auto key_merge_symbols<T>
+#else
+inline constexpr auto key_merge_symbols<T, std::enable_if_t<is_same_cvr_v<T, noext00>>>
+#endif
+    = [](auto &&, const symbol_idx_map<symbol_set> &, const symbol_set &) constexpr noexcept
+{
+    return 42;
+};
+
+} // namespace piranha::customisation
+
+TEST_CASE("key_merge_symbols_test")
+{
+    REQUIRE(!is_symbols_mergeable_key_v<void>);
+    REQUIRE(!is_symbols_mergeable_key_v<int>);
+    REQUIRE(!is_symbols_mergeable_key_v<double>);
+    REQUIRE(!is_symbols_mergeable_key_v<no_kms00>);
+
+    REQUIRE(is_symbols_mergeable_key_v<ns::kms00>);
+    REQUIRE(is_symbols_mergeable_key_v<ns::kms00 &>);
+    REQUIRE(is_symbols_mergeable_key_v<const ns::kms00 &>);
+
+    REQUIRE(!is_symbols_mergeable_key_v<ns::no_kms00>);
+    REQUIRE(!is_symbols_mergeable_key_v<ns::no_kms00 &>);
+    REQUIRE(!is_symbols_mergeable_key_v<const ns::no_kms00 &>);
+
+    REQUIRE(!is_symbols_mergeable_key_v<ns::kms01>);
+    REQUIRE(is_symbols_mergeable_key_v<ns::kms01 &>);
+    REQUIRE(!is_symbols_mergeable_key_v<const ns::kms01 &>);
+
+    REQUIRE(is_symbols_mergeable_key_v<ext00>);
+    REQUIRE(is_symbols_mergeable_key_v<ext00 &>);
+    REQUIRE(is_symbols_mergeable_key_v<const ext00 &>);
+
+    REQUIRE(!is_symbols_mergeable_key_v<ext01>);
+    REQUIRE(is_symbols_mergeable_key_v<ext01 &>);
+    REQUIRE(is_symbols_mergeable_key_v<const ext01 &>);
+
+    REQUIRE(!is_symbols_mergeable_key_v<noext00>);
+    REQUIRE(!is_symbols_mergeable_key_v<noext00 &>);
+    REQUIRE(!is_symbols_mergeable_key_v<const noext00 &>);
+
+#if defined(PIRANHA_HAVE_CONCEPTS)
+    REQUIRE(!SymbolsMergeableKey<void>);
+    REQUIRE(!SymbolsMergeableKey<int>);
+    REQUIRE(!SymbolsMergeableKey<double>);
+    REQUIRE(!SymbolsMergeableKey<no_kms00>);
+
+    REQUIRE(SymbolsMergeableKey<ns::kms00>);
+    REQUIRE(SymbolsMergeableKey<ns::kms00 &>);
+    REQUIRE(SymbolsMergeableKey<const ns::kms00 &>);
+
+    REQUIRE(!SymbolsMergeableKey<ns::no_kms00>);
+    REQUIRE(!SymbolsMergeableKey<ns::no_kms00 &>);
+    REQUIRE(!SymbolsMergeableKey<const ns::no_kms00 &>);
+
+    REQUIRE(!SymbolsMergeableKey<ns::kms01>);
+    REQUIRE(SymbolsMergeableKey<ns::kms01 &>);
+    REQUIRE(!SymbolsMergeableKey<const ns::kms01 &>);
+
+    REQUIRE(SymbolsMergeableKey<ext00>);
+    REQUIRE(SymbolsMergeableKey<ext00 &>);
+    REQUIRE(SymbolsMergeableKey<const ext00 &>);
+
+    REQUIRE(!SymbolsMergeableKey<ext01>);
+    REQUIRE(SymbolsMergeableKey<ext01 &>);
+    REQUIRE(SymbolsMergeableKey<const ext01 &>);
+
+    REQUIRE(!SymbolsMergeableKey<noext00>);
+    REQUIRE(!SymbolsMergeableKey<noext00 &>);
+    REQUIRE(!SymbolsMergeableKey<const noext00 &>);
+#endif
+}
