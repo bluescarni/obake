a0da131937b1db4b83faf9176d150578daba3188
Francesco Biscani <bluescarni@gmail.com>
2019-06-14 10:35:34 +0200

    Initial files for cf_stream_insert.

---
+++ include/piranha/cf/cf_stream_insert.hpp
@@ -0,0 +1,103 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the piranha library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef PIRANHA_CF_CF_STREAM_INSERT_HPP
+#define PIRANHA_CF_CF_STREAM_INSERT_HPP
+
+#include <ostream>
+#include <utility>
+
+#include <piranha/config.hpp>
+#include <piranha/detail/not_implemented.hpp>
+#include <piranha/detail/priority_tag.hpp>
+#include <piranha/detail/ss_func_forward.hpp>
+#include <piranha/type_traits.hpp>
+
+namespace piranha
+{
+
+namespace customisation
+{
+
+// External customisation point for piranha::cf_stream_insert().
+template <typename T
+#if !defined(PIRANHA_HAVE_CONCEPTS)
+          ,
+          typename = void
+#endif
+          >
+inline constexpr auto cf_stream_insert = not_implemented;
+
+} // namespace customisation
+
+namespace detail
+{
+
+// Highest priority: explicit user override in the external customisation namespace.
+template <typename T>
+constexpr auto cf_stream_insert_impl(::std::ostream &os, T &&x, priority_tag<2>)
+    PIRANHA_SS_FORWARD_FUNCTION((customisation::cf_stream_insert<T &&>)(os, ::std::forward<T>(x)));
+
+// Unqualified function call implementation.
+template <typename T>
+constexpr auto cf_stream_insert_impl(::std::ostream &os, T &&x, priority_tag<1>)
+    PIRANHA_SS_FORWARD_FUNCTION(cf_stream_insert(os, ::std::forward<T>(x)));
+
+// Default implementation.
+template <typename T>
+constexpr auto cf_stream_insert_impl(::std::ostream &os, T &&x, priority_tag<0>)
+    PIRANHA_SS_FORWARD_FUNCTION(void(os << ::std::forward<T>(x)));
+
+} // namespace detail
+
+#if defined(_MSC_VER)
+
+struct cf_stream_insert_msvc {
+    template <typename T>
+    constexpr auto operator()(::std::ostream &os, T &&x) const
+        PIRANHA_SS_FORWARD_MEMBER_FUNCTION(detail::cf_stream_insert_impl(os, ::std::forward<T>(x),
+                                                                         detail::priority_tag<2>{}))
+};
+
+inline constexpr auto cf_stream_insert = cf_stream_insert_msvc{};
+
+#else
+
+inline constexpr auto cf_stream_insert = [](::std::ostream & os, auto &&x) PIRANHA_SS_FORWARD_LAMBDA(
+    detail::cf_stream_insert_impl(os, ::std::forward<decltype(x)>(x), detail::priority_tag<2>{}));
+
+#endif
+
+namespace detail
+{
+
+template <typename T>
+using cf_stream_insert_t
+    = decltype(::piranha::cf_stream_insert(::std::declval<::std::ostream &>(), ::std::declval<T>()));
+
+}
+
+template <typename T>
+using is_stream_insertable_cf = is_detected<detail::cf_stream_insert_t, T>;
+
+template <typename T>
+inline constexpr bool is_stream_insertable_cf_v = is_stream_insertable_cf<T>::value;
+
+#if defined(PIRANHA_HAVE_CONCEPTS)
+
+template <typename T>
+PIRANHA_CONCEPT_DECL StreamInsertableCf = requires(::std::ostream &os, T &&x)
+{
+    ::piranha::cf_stream_insert(os, ::std::forward<T>(x));
+};
+
+#endif
+
+} // namespace piranha
+
+#endif
---
+++ test/cf_cf_stream_insert.cpp
@@ -0,0 +1,153 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the piranha library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <piranha/cf/cf_stream_insert.hpp>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <ostream>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+#include <piranha/config.hpp>
+#include <piranha/type_traits.hpp>
+
+using namespace piranha;
+
+// ADL-based implementations.
+namespace ns
+{
+
+struct si00 {
+};
+
+void cf_stream_insert(std::ostream &os, const si00 &);
+
+struct si01 {
+};
+
+// Disable certain overloads.
+void cf_stream_insert(std::ostream &os, si01 &);
+
+struct nsi00 {
+};
+
+} // namespace ns
+
+struct ext_si00 {
+};
+
+struct ext_si01 {
+};
+
+namespace piranha::customisation
+{
+
+template <typename T>
+#if defined(PIRANHA_HAVE_CONCEPTS)
+requires SameCvr<T, ext_si00> inline constexpr auto cf_stream_insert<T>
+#else
+inline constexpr auto cf_stream_insert<T, std::enable_if_t<is_same_cvr_v<T, ext_si00>>>
+#endif
+    = [](std::ostream &, auto &&) constexpr noexcept
+{
+    return true;
+};
+
+template <typename T>
+#if defined(PIRANHA_HAVE_CONCEPTS)
+requires SameCvr<T, ext_si01> inline constexpr auto cf_stream_insert<T>
+#else
+inline constexpr auto cf_stream_insert<T, std::enable_if_t<is_same_cvr_v<T, ext_si01>>>
+#endif
+    = [](std::ostream &, auto &) constexpr noexcept
+{
+    return true;
+};
+
+} // namespace piranha::customisation
+
+TEST_CASE("cf_stream_insert_test")
+{
+    REQUIRE(!is_stream_insertable_cf_v<void>);
+
+    REQUIRE(is_stream_insertable_cf_v<int>);
+    REQUIRE(is_stream_insertable_cf_v<int &>);
+    REQUIRE(is_stream_insertable_cf_v<const int &>);
+    REQUIRE(is_stream_insertable_cf_v<std::string>);
+    REQUIRE(is_stream_insertable_cf_v<std::string &>);
+    REQUIRE(is_stream_insertable_cf_v<const std::string &>);
+
+    std::ostringstream oss1, oss2;
+    cf_stream_insert(oss1, 42);
+    oss2 << 42;
+    REQUIRE(oss1.str() == oss2.str());
+
+    REQUIRE(is_stream_insertable_cf_v<ns::si00>);
+    REQUIRE(is_stream_insertable_cf_v<ns::si00 &>);
+    REQUIRE(is_stream_insertable_cf_v<const ns::si00 &>);
+    REQUIRE(is_stream_insertable_cf_v<ns::si00 &&>);
+
+    REQUIRE(!is_stream_insertable_cf_v<ns::si01>);
+    REQUIRE(is_stream_insertable_cf_v<ns::si01 &>);
+    REQUIRE(!is_stream_insertable_cf_v<const ns::si01 &>);
+    REQUIRE(!is_stream_insertable_cf_v<ns::si01 &&>);
+
+    REQUIRE(!is_stream_insertable_cf_v<ns::nsi00>);
+    REQUIRE(!is_stream_insertable_cf_v<ns::nsi00 &>);
+    REQUIRE(!is_stream_insertable_cf_v<const ns::nsi00 &>);
+    REQUIRE(!is_stream_insertable_cf_v<ns::nsi00 &&>);
+
+    REQUIRE(is_stream_insertable_cf_v<ext_si00>);
+    REQUIRE(is_stream_insertable_cf_v<ext_si00 &>);
+    REQUIRE(is_stream_insertable_cf_v<const ext_si00 &>);
+    REQUIRE(is_stream_insertable_cf_v<ext_si00 &&>);
+
+    REQUIRE(!is_stream_insertable_cf_v<ext_si01>);
+    REQUIRE(is_stream_insertable_cf_v<ext_si01 &>);
+    REQUIRE(is_stream_insertable_cf_v<const ext_si01 &>);
+    REQUIRE(!is_stream_insertable_cf_v<ext_si01 &&>);
+
+#if defined(PIRANHA_HAVE_CONCEPTS)
+    REQUIRE(!StreamInsertableCf<void>);
+
+    REQUIRE(StreamInsertableCf<int>);
+    REQUIRE(StreamInsertableCf<int &>);
+    REQUIRE(StreamInsertableCf<const int &>);
+    REQUIRE(StreamInsertableCf<std::string>);
+    REQUIRE(StreamInsertableCf<std::string &>);
+    REQUIRE(StreamInsertableCf<const std::string &>);
+
+    REQUIRE(StreamInsertableCf<ns::si00>);
+    REQUIRE(StreamInsertableCf<ns::si00 &>);
+    REQUIRE(StreamInsertableCf<const ns::si00 &>);
+    REQUIRE(StreamInsertableCf<ns::si00 &&>);
+
+    REQUIRE(!StreamInsertableCf<ns::si01>);
+    REQUIRE(StreamInsertableCf<ns::si01 &>);
+    REQUIRE(!StreamInsertableCf<const ns::si01 &>);
+    REQUIRE(!StreamInsertableCf<ns::si01 &&>);
+
+    REQUIRE(!StreamInsertableCf<ns::nsi00>);
+    REQUIRE(!StreamInsertableCf<ns::nsi00 &>);
+    REQUIRE(!StreamInsertableCf<const ns::nsi00 &>);
+    REQUIRE(!StreamInsertableCf<ns::nsi00 &&>);
+
+    REQUIRE(StreamInsertableCf<ext_si00>);
+    REQUIRE(StreamInsertableCf<ext_si00 &>);
+    REQUIRE(StreamInsertableCf<const ext_si00 &>);
+    REQUIRE(StreamInsertableCf<ext_si00 &&>);
+
+    REQUIRE(!StreamInsertableCf<ext_si01>);
+    REQUIRE(StreamInsertableCf<ext_si01 &>);
+    REQUIRE(StreamInsertableCf<const ext_si01 &>);
+    REQUIRE(!StreamInsertableCf<ext_si01 &&>);
+#endif
+}
