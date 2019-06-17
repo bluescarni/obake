5d8df1dfb2ed24db1f39f793a5ff56fd8d93ea73
Francesco Biscani <bluescarni@gmail.com>
2019-06-17 14:30:50 +0200

    Remove obsolete TODO items.

--- include/piranha/series.hpp
+++ include/piranha/series.hpp
@@ -501,8 +501,6 @@ struct series_rref_clearer {
 } // namespace detail
 
 // TODO: document that moved-from series are destructible and assignable.
-// TODO: test singular iterators.
-// TODO: check construction of const iterators from mutable ones.
 // TODO: test term annihilation during add/sub.
 #if defined(PIRANHA_HAVE_CONCEPTS)
 template <Key K, Cf C, typename Tag>
