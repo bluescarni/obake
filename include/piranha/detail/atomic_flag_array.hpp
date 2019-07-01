3664577da42118e8418dae47e1f05690ce4097d4
Francesco Biscani <bluescarni@gmail.com>
2019-07-01 13:37:05 +0200

    Import the atomic utils from older piranha.

---
+++ include/piranha/detail/atomic_flag_array.hpp
@@ -0,0 +1,59 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the piranha library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef PIRANHA_DETAIL_ATOMIC_FLAG_ARRAY_HPP
+#define PIRANHA_DETAIL_ATOMIC_FLAG_ARRAY_HPP
+
+#include <atomic>
+#include <cstddef>
+#include <memory>
+
+#include <piranha/detail/visibility.hpp>
+
+namespace piranha::detail
+{
+
+// Helper to manage an array of atomic flags.
+// The flags will all be cleared upon construction
+// from a size.
+class PIRANHA_DLL_PUBLIC atomic_flag_array
+{
+public:
+    using value_type = ::std::atomic_flag;
+    using size_type = ::std::size_t;
+
+    explicit atomic_flag_array(size_type);
+    // atomic_flag is guaranteed to have a trivial dtor,
+    // so we can just let unique_ptr free the storage. No need
+    // for custom dtor.
+    // http://en.cppreference.com/w/cpp/atomic/atomic
+    // Delete explicitly all other ctors/assignment operators.
+    atomic_flag_array() = delete;
+    atomic_flag_array(const atomic_flag_array &) = delete;
+    atomic_flag_array(atomic_flag_array &&) = delete;
+    atomic_flag_array &operator=(const atomic_flag_array &) = delete;
+    atomic_flag_array &operator=(atomic_flag_array &&) = delete;
+
+    // The accessors.
+    value_type &operator[](const size_type &i)
+    {
+        return *reinterpret_cast<value_type *>(m_ptr.get() + sizeof(value_type) * i);
+    }
+    const value_type &operator[](const size_type &i) const
+    {
+        return *reinterpret_cast<const value_type *>(m_ptr.get() + sizeof(value_type) * i);
+    }
+
+private:
+    ::std::unique_ptr<unsigned char[]> m_ptr;
+    const size_type m_size;
+};
+
+} // namespace piranha::detail
+
+#endif
---
+++ include/piranha/detail/atomic_lock_guard.hpp
@@ -0,0 +1,47 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the piranha library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef PIRANHA_DETAIL_ATOMIC_LOCK_GUARD_HPP
+#define PIRANHA_DETAIL_ATOMIC_LOCK_GUARD_HPP
+
+#include <atomic>
+
+namespace piranha::detail
+{
+
+// A simple spinlock built on top of std::atomic_flag. See for reference:
+// http://en.cppreference.com/w/cpp/atomic/atomic_flag
+// http://stackoverflow.com/questions/26583433/c11-implementation-of-spinlock-using-atomic
+// The memory order specification is to squeeze out some extra performance with respect to the
+// default behaviour of atomic types.
+class atomic_lock_guard
+{
+public:
+    explicit atomic_lock_guard(::std::atomic_flag &af) : m_af(af)
+    {
+        while (m_af.test_and_set(::std::memory_order_acquire)) {
+        }
+    }
+    ~atomic_lock_guard()
+    {
+        m_af.clear(::std::memory_order_release);
+    }
+    // Delete explicitly all other ctors/assignment operators.
+    atomic_lock_guard() = delete;
+    atomic_lock_guard(const atomic_lock_guard &) = delete;
+    atomic_lock_guard(atomic_lock_guard &&) = delete;
+    atomic_lock_guard &operator=(const atomic_lock_guard &) = delete;
+    atomic_lock_guard &operator=(atomic_lock_guard &&) = delete;
+
+private:
+    ::std::atomic_flag &m_af;
+};
+
+} // namespace piranha::detail
+
+#endif
