8f9983f559ebc64a14f0306e5e21d165ff25c29e
Francesco Biscani <bluescarni@gmail.com>
2019-09-03 06:44:52 +0200

    Tentative ASAN fix.

--- tools/circleci_bionic_gcc7_asan.sh
+++ tools/circleci_bionic_gcc7_asan.sh
@@ -21,8 +21,9 @@ bash ../tools/circleci_install_abseil.sh
 # GCC build.
 cmake ../ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_PREFIX_PATH=~/.local -DPIRANHA_BUILD_TESTS=YES -DCMAKE_CXX_FLAGS="-fsanitize=address" -DPIRANHA_WITH_LIBBACKTRACE=YES
 make -j2 VERBOSE=1
-# Run the tests.
-ctest -V
+# Run the tests. Enable the custom suppression file for ASAN
+# in order to suppress spurious warnings from TBB code.
+LSAN_OPTIONS=suppressions=/home/circleci/project/tools/lsan.supp ctest -V
 
 set +e
 set +x
