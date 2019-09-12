51a7e3ac8980a336da3cbbfa6e3f9582761d25f3
Francesco Biscani <bluescarni@gmail.com>
2019-09-12 15:44:41 +0200

    Introduce symbol_idx_set, small internal docs.

--- include/piranha/symbols.hpp
+++ include/piranha/symbols.hpp
@@ -20,9 +20,14 @@
 namespace piranha
 {
 
+// Set of symbols.
 using symbol_set = ::boost::container::flat_set<::std::string>;
 
+// Unsigned integral type for indexing into a symbol_set.
 using symbol_idx = symbol_set::size_type;
+
+// Set of symbol indices.
+using symbol_idx_set = ::boost::container::flat_set<symbol_idx>;
 
 // Map of symbol indices. This sorted data structure maps
 // symbol_idx instances to instances of T.
