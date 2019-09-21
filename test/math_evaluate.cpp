acd901143ff979f08dc29afac5bedc3a750f383f
Francesco Biscani <bluescarni@gmail.com>
2019-09-21 16:26:30 +0200

    Testing for piranha::evaluate().

--- test/math_evaluate.cpp
+++ test/math_evaluate.cpp
@@ -6,12 +6,12 @@
 // Public License v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// #include <limits>
-// #include <type_traits>
+#include <type_traits>
 
 #include <piranha/config.hpp>
 #include <piranha/math/evaluate.hpp>
-// #include <piranha/type_traits.hpp>
+#include <piranha/symbols.hpp>
+#include <piranha/type_traits.hpp>
 
 #include "catch.hpp"
 
@@ -19,124 +19,48 @@ using namespace piranha;
 
 TEST_CASE("evaluate_arith")
 {
-    REQUIRE(!piranha::is_evaluable_v<void, void>);
-    REQUIRE(!piranha::is_evaluable_v<void, int>);
-    REQUIRE(!piranha::is_evaluable_v<int, void>);
-    REQUIRE(!piranha::is_evaluable_v<int &, void>);
-    REQUIRE(!piranha::is_evaluable_v<const int &, void>);
-    REQUIRE(!piranha::is_evaluable_v<const int, void>);
-}
+    REQUIRE(!is_evaluable_v<void, void>);
+    REQUIRE(!is_evaluable_v<void, int>);
+    REQUIRE(!is_evaluable_v<int, void>);
+    REQUIRE(!is_evaluable_v<int &, void>);
+    REQUIRE(!is_evaluable_v<const int &, void>);
+    REQUIRE(!is_evaluable_v<const int, void>);
 
-#if 0
-
-TEST_CASE("is_zero_mp++_int")
-{
-    using int_t = mppp::integer<1>;
-
-    REQUIRE(piranha::is_zero_testable_v<int_t>);
-    REQUIRE(piranha::is_zero_testable_v<int_t &>);
-    REQUIRE(piranha::is_zero_testable_v<const int_t &>);
-    REQUIRE(piranha::is_zero_testable_v<int_t &&>);
+    REQUIRE(is_evaluable_v<int, int>);
+    REQUIRE(is_evaluable_v<int &, int>);
+    REQUIRE(is_evaluable_v<const int &, int>);
+    REQUIRE(is_evaluable_v<const int, int>);
+    REQUIRE(!is_evaluable_v<int, int &>);
+    REQUIRE(!is_evaluable_v<int, const int>);
 
 #if defined(PIRANHA_HAVE_CONCEPTS)
-    REQUIRE(piranha::ZeroTestable<int_t>);
-    REQUIRE(piranha::ZeroTestable<int_t &>);
-    REQUIRE(piranha::ZeroTestable<const int_t &>);
-    REQUIRE(piranha::ZeroTestable<int_t &&>);
-#endif
+    REQUIRE(!Evaluable<void, void>);
+    REQUIRE(!Evaluable<void, int>);
+    REQUIRE(!Evaluable<int, void>);
+    REQUIRE(!Evaluable<int &, void>);
+    REQUIRE(!Evaluable<const int &, void>);
+    REQUIRE(!Evaluable<const int, void>);
 
-    REQUIRE(piranha::is_zero(int_t{}));
-    REQUIRE(!piranha::is_zero(int_t{-1}));
+    REQUIRE(Evaluable<int, int>);
+    REQUIRE(Evaluable<int &, int>);
+    REQUIRE(Evaluable<const int &, int>);
+    REQUIRE(Evaluable<const int, int>);
+    REQUIRE(!Evaluable<int, int &>);
+    REQUIRE(!Evaluable<int, const int>);
+#endif
 }
 
-TEST_CASE("is_zero_mp++_rat")
-{
-    using rat_t = mppp::rational<1>;
-
-    REQUIRE(piranha::is_zero_testable_v<rat_t>);
-    REQUIRE(piranha::is_zero_testable_v<rat_t &>);
-    REQUIRE(piranha::is_zero_testable_v<const rat_t &>);
-    REQUIRE(piranha::is_zero_testable_v<rat_t &&>);
-
-#if defined(PIRANHA_HAVE_CONCEPTS)
-    REQUIRE(piranha::ZeroTestable<rat_t>);
-    REQUIRE(piranha::ZeroTestable<rat_t &>);
-    REQUIRE(piranha::ZeroTestable<const rat_t &>);
-    REQUIRE(piranha::ZeroTestable<rat_t &&>);
-#endif
-
-    REQUIRE(piranha::is_zero(rat_t{}));
-    REQUIRE(!piranha::is_zero(rat_t{-1, 45}));
-}
-
-#if defined(MPPP_WITH_MPFR)
-
-TEST_CASE("is_zero_mp++_real")
-{
-    REQUIRE(piranha::is_zero_testable_v<mppp::real>);
-    REQUIRE(piranha::is_zero_testable_v<mppp::real &>);
-    REQUIRE(piranha::is_zero_testable_v<const mppp::real &>);
-    REQUIRE(piranha::is_zero_testable_v<mppp::real &&>);
-
-#if defined(PIRANHA_HAVE_CONCEPTS)
-    REQUIRE(piranha::ZeroTestable<mppp::real>);
-    REQUIRE(piranha::ZeroTestable<mppp::real &>);
-    REQUIRE(piranha::ZeroTestable<const mppp::real &>);
-    REQUIRE(piranha::ZeroTestable<mppp::real &&>);
-#endif
-
-    REQUIRE(piranha::is_zero(mppp::real{}));
-    REQUIRE(!piranha::is_zero(mppp::real{42}));
-    REQUIRE(!piranha::is_zero(mppp::real{"inf", 100}));
-    REQUIRE(!piranha::is_zero(mppp::real{"nan", 100}));
-}
-
-#endif
-
-#if defined(MPPP_WITH_QUADMATH)
-
-TEST_CASE("is_zero_mp++_real128")
-{
-    REQUIRE(piranha::is_zero_testable_v<mppp::real128>);
-    REQUIRE(piranha::is_zero_testable_v<mppp::real128 &>);
-    REQUIRE(piranha::is_zero_testable_v<const mppp::real128 &>);
-    REQUIRE(piranha::is_zero_testable_v<mppp::real128 &&>);
-
-#if defined(PIRANHA_HAVE_CONCEPTS)
-    REQUIRE(piranha::ZeroTestable<mppp::real128>);
-    REQUIRE(piranha::ZeroTestable<mppp::real128 &>);
-    REQUIRE(piranha::ZeroTestable<const mppp::real128 &>);
-    REQUIRE(piranha::ZeroTestable<mppp::real128 &&>);
-#endif
-
-    REQUIRE(piranha::is_zero(mppp::real128{}));
-    REQUIRE(!piranha::is_zero(mppp::real128{42}));
-    REQUIRE(!piranha::is_zero(mppp::real128{"inf"}));
-    REQUIRE(!piranha::is_zero(mppp::real128{"nan"}));
-
-    // Verify constexpr.
-    [[maybe_unused]] foo_b<piranha::is_zero(mppp::real128{})> tmp;
-}
-
-#endif
-
-struct no_is_zero_0 {
+struct evaluate_base {
 };
-
-// Wrong return type in the ADL implementation.
-struct no_is_zero_1 {
-};
-
-void is_zero(const no_is_zero_1 &);
 
 // OK ADL implementation.
-struct is_zero_0 {
+struct evaluate_0 {
 };
 
-int is_zero(const is_zero_0 &);
+int evaluate(const evaluate_0 &, const symbol_map<int> &);
 
 // External customisation point.
-struct is_zero_1 {
+struct evaluate_1 {
 };
 
 namespace piranha::customisation
@@ -144,31 +68,38 @@ namespace piranha::customisation
 
 template <typename T>
 #if defined(PIRANHA_HAVE_CONCEPTS)
-requires SameCvr<T, is_zero_1> inline constexpr auto is_zero<T>
+requires SameCvr<T, evaluate_1> inline constexpr auto evaluate<T, double>
 #else
-inline constexpr auto is_zero<T, std::enable_if_t<is_same_cvr_v<T, is_zero_1>>>
+inline constexpr auto evaluate<T, double, std::enable_if_t<is_same_cvr_v<T, evaluate_1>>>
 #endif
-    = [](auto &&) constexpr noexcept
+    = [](auto &&, const symbol_map<double> &) constexpr noexcept
 {
     return true;
 };
 
 } // namespace piranha::customisation
 
-TEST_CASE("is_zero_custom")
+TEST_CASE("evaluate_custom")
 {
     // Check type-traits/concepts.
-    REQUIRE(!piranha::is_zero_testable_v<no_is_zero_0>);
-    REQUIRE(!piranha::is_zero_testable_v<no_is_zero_1>);
-    REQUIRE(piranha::is_zero_testable_v<is_zero_0>);
-    REQUIRE(piranha::is_zero_testable_v<is_zero_1>);
+    REQUIRE(is_evaluable_v<evaluate_base, int>);
+    REQUIRE(!is_evaluable_v<evaluate_base, int &>);
+    REQUIRE(is_evaluable_v<evaluate_0, int>);
+    REQUIRE(is_evaluable_v<evaluate_0, double>);
+    REQUIRE(is_evaluable_v<evaluate_1, double>);
+    REQUIRE(is_evaluable_v<evaluate_1, int>);
+
+    REQUIRE(std::is_same_v<int, decltype(piranha::evaluate(evaluate_0{}, symbol_map<int>{}))>);
+    REQUIRE(std::is_same_v<evaluate_0, decltype(piranha::evaluate(evaluate_0{}, symbol_map<double>{}))>);
+    REQUIRE(std::is_same_v<evaluate_1, decltype(piranha::evaluate(evaluate_1{}, symbol_map<int>{}))>);
+    REQUIRE(std::is_same_v<bool, decltype(piranha::evaluate(evaluate_1{}, symbol_map<double>{}))>);
 
 #if defined(PIRANHA_HAVE_CONCEPTS)
-    REQUIRE(!piranha::ZeroTestable<no_is_zero_0>);
-    REQUIRE(!piranha::ZeroTestable<no_is_zero_1>);
-    REQUIRE(piranha::ZeroTestable<is_zero_0>);
-    REQUIRE(piranha::ZeroTestable<is_zero_1>);
+    REQUIRE(Evaluable<evaluate_base, int>);
+    REQUIRE(!Evaluable<evaluate_base, int &>);
+    REQUIRE(Evaluable<evaluate_0, int>);
+    REQUIRE(Evaluable<evaluate_0, double>);
+    REQUIRE(Evaluable<evaluate_1, double>);
+    REQUIRE(Evaluable<evaluate_1, int>);
 #endif
 }
-
-#endif
