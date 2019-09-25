8a9a2848a866e5050f143f123f8bed1ea905a5c6
Francesco Biscani <bluescarni@gmail.com>
2019-09-25 16:47:12 +0200

    key_trim() machinery.

---
+++ include/obake/key/key_trim.hpp
@@ -0,0 +1,110 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the obake library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OBAKE_KEY_KEY_TRIM_HPP
+#define OBAKE_KEY_KEY_TRIM_HPP
+
+#include <type_traits>
+#include <utility>
+
+#include <obake/config.hpp>
+#include <obake/detail/not_implemented.hpp>
+#include <obake/detail/priority_tag.hpp>
+#include <obake/detail/ss_func_forward.hpp>
+#include <obake/symbols.hpp>
+#include <obake/type_traits.hpp>
+
+namespace obake
+{
+
+namespace customisation
+{
+
+// External customisation point for obake::key_trim().
+template <typename T
+#if !defined(OBAKE_HAVE_CONCEPTS)
+          ,
+          typename = void
+#endif
+          >
+inline constexpr auto key_trim = not_implemented;
+
+} // namespace customisation
+
+namespace detail
+{
+
+// Highest priority: explicit user override in the external customisation namespace.
+template <typename T>
+constexpr auto key_trim_impl(T &&x, const symbol_idx_set &si, const symbol_set &ss, priority_tag<1>)
+    OBAKE_SS_FORWARD_FUNCTION((customisation::key_trim<T &&>)(::std::forward<T>(x), si, ss));
+
+// Unqualified function call implementation.
+template <typename T>
+constexpr auto key_trim_impl(T &&x, const symbol_idx_set &si, const symbol_set &ss, priority_tag<0>)
+    OBAKE_SS_FORWARD_FUNCTION(key_trim(::std::forward<T>(x), si, ss));
+
+// Machinery to enable the key_trim implementation only if the return
+// type is the same as the input type (after cvref removal).
+template <typename T>
+using key_trim_impl_ret_t
+    = decltype(detail::key_trim_impl(::std::declval<T>(), ::std::declval<const symbol_idx_set &>(),
+                                     ::std::declval<const symbol_set &>(), priority_tag<1>{}));
+
+template <typename T,
+          ::std::enable_if_t<::std::is_same_v<remove_cvref_t<T>, detected_t<key_trim_impl_ret_t, T>>, int> = 0>
+constexpr auto key_trim_impl_with_ret_check(T &&x, const symbol_idx_set &si, const symbol_set &ss)
+    OBAKE_SS_FORWARD_FUNCTION(detail::key_trim_impl(::std::forward<T>(x), si, ss, priority_tag<1>{}));
+
+} // namespace detail
+
+#if defined(_MSC_VER)
+
+struct key_trim_msvc {
+    template <typename T>
+    constexpr auto operator()(T &&x, const symbol_idx_set &si, const symbol_set &ss) const
+        OBAKE_SS_FORWARD_MEMBER_FUNCTION(detail::key_trim_impl_with_ret_check(::std::forward<T>(x), si, ss))
+};
+
+inline constexpr auto key_trim = key_trim_msvc{};
+
+#else
+
+inline constexpr auto key_trim = [](auto &&x, const symbol_idx_set &si, const symbol_set &ss)
+    OBAKE_SS_FORWARD_LAMBDA(detail::key_trim_impl_with_ret_check(::std::forward<decltype(x)>(x), si, ss));
+
+#endif
+
+namespace detail
+{
+
+template <typename T>
+using key_trim_t = decltype(::obake::key_trim(::std::declval<T>(), ::std::declval<const symbol_idx_set &>(),
+                                              ::std::declval<const symbol_set &>()));
+
+}
+
+template <typename T>
+using is_trimmable_key = is_detected<detail::key_trim_t, T>;
+
+template <typename T>
+inline constexpr bool is_trimmable_key_v = is_trimmable_key<T>::value;
+
+#if defined(OBAKE_HAVE_CONCEPTS)
+
+template <typename T>
+OBAKE_CONCEPT_DECL TrimmableKey = requires(T &&x, const symbol_idx_set &si, const symbol_set &ss)
+{
+    ::obake::key_trim(::std::forward<T>(x), si, ss);
+};
+
+#endif
+
+} // namespace obake
+
+#endif
---
+++ test/key_key_trim.cpp
@@ -0,0 +1,194 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the obake library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <type_traits>
+
+#include <obake/config.hpp>
+#include <obake/key/key_trim.hpp>
+#include <obake/symbols.hpp>
+#include <obake/type_traits.hpp>
+
+#include "catch.hpp"
+
+using namespace obake;
+
+struct notrim_00 {
+};
+
+namespace ns
+{
+
+struct trim_00 {
+};
+
+trim_00 key_trim(const trim_00 &, const symbol_idx_set &, const symbol_set &);
+
+struct trim_01 {
+};
+
+trim_01 key_trim(trim_01 &, const symbol_idx_set &, const symbol_set &);
+
+struct trim_02 {
+};
+
+// Wrong return type.
+void key_trim(const trim_02 &, const symbol_idx_set &, const symbol_set &);
+
+struct trim_03 {
+};
+
+// Wrong signature.
+int key_trim(const trim_03 &);
+
+} // namespace ns
+
+struct trim_ext {
+};
+
+struct notrim_ext_00 {
+};
+
+struct notrim_ext_01 {
+};
+
+namespace obake::customisation
+{
+
+template <typename T>
+#if defined(OBAKE_HAVE_CONCEPTS)
+requires SameCvr<T, trim_ext> inline constexpr auto key_trim<T>
+#else
+inline constexpr auto key_trim<T, std::enable_if_t<is_same_cvr_v<T, trim_ext>>>
+#endif
+    = [](auto &&, const symbol_idx_set &, const symbol_set &) constexpr noexcept
+{
+    return trim_ext{};
+};
+
+template <typename T>
+#if defined(OBAKE_HAVE_CONCEPTS)
+requires SameCvr<T, notrim_ext_00> inline constexpr auto key_trim<T>
+#else
+inline constexpr auto key_trim<T, std::enable_if_t<is_same_cvr_v<T, notrim_ext_00>>>
+#endif
+    = [](auto &&, const symbol_idx_set &, const symbol_set &) constexpr noexcept
+{
+    return 1;
+};
+
+template <typename T>
+#if defined(OBAKE_HAVE_CONCEPTS)
+requires SameCvr<T, notrim_ext_01> inline constexpr auto key_trim<T>
+#else
+inline constexpr auto key_trim<T, std::enable_if_t<is_same_cvr_v<T, notrim_ext_01>>>
+#endif
+    = [](notrim_ext_01 &, const symbol_idx_set &, const symbol_set &) constexpr noexcept
+{
+    return notrim_ext_01{};
+};
+
+} // namespace obake::customisation
+
+TEST_CASE("key_trim_test")
+{
+    REQUIRE(!is_trimmable_key_v<void>);
+
+    REQUIRE(!is_trimmable_key_v<int>);
+    REQUIRE(!is_trimmable_key_v<int &>);
+    REQUIRE(!is_trimmable_key_v<const int &>);
+    REQUIRE(!is_trimmable_key_v<int &&>);
+
+    REQUIRE(!is_trimmable_key_v<notrim_00>);
+    REQUIRE(!is_trimmable_key_v<notrim_00 &>);
+    REQUIRE(!is_trimmable_key_v<const notrim_00 &>);
+    REQUIRE(!is_trimmable_key_v<notrim_00 &&>);
+
+    REQUIRE(is_trimmable_key_v<ns::trim_00>);
+    REQUIRE(is_trimmable_key_v<ns::trim_00 &>);
+    REQUIRE(is_trimmable_key_v<const ns::trim_00 &>);
+    REQUIRE(is_trimmable_key_v<ns::trim_00 &&>);
+
+    REQUIRE(!is_trimmable_key_v<ns::trim_01>);
+    REQUIRE(is_trimmable_key_v<ns::trim_01 &>);
+    REQUIRE(!is_trimmable_key_v<const ns::trim_01 &>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_01 &&>);
+
+    REQUIRE(!is_trimmable_key_v<ns::trim_02>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_02 &>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_02 &>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_02 &&>);
+
+    REQUIRE(!is_trimmable_key_v<ns::trim_03>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_03 &>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_03 &>);
+    REQUIRE(!is_trimmable_key_v<ns::trim_03 &&>);
+
+    REQUIRE(is_trimmable_key_v<trim_ext>);
+    REQUIRE(is_trimmable_key_v<trim_ext &>);
+    REQUIRE(is_trimmable_key_v<const trim_ext &>);
+    REQUIRE(is_trimmable_key_v<trim_ext &&>);
+
+    REQUIRE(!is_trimmable_key_v<notrim_ext_00>);
+    REQUIRE(!is_trimmable_key_v<notrim_ext_00 &>);
+    REQUIRE(!is_trimmable_key_v<const notrim_ext_00 &>);
+    REQUIRE(!is_trimmable_key_v<notrim_ext_00 &&>);
+
+    REQUIRE(!is_trimmable_key_v<notrim_ext_01>);
+    REQUIRE(is_trimmable_key_v<notrim_ext_01 &>);
+    REQUIRE(!is_trimmable_key_v<const notrim_ext_01 &>);
+    REQUIRE(!is_trimmable_key_v<notrim_ext_01 &&>);
+
+#if defined(OBAKE_HAVE_CONCEPTS)
+    REQUIRE(!TrimmableKey<void>);
+
+    REQUIRE(!TrimmableKey<int>);
+    REQUIRE(!TrimmableKey<int &>);
+    REQUIRE(!TrimmableKey<const int &>);
+    REQUIRE(!TrimmableKey<int &&>);
+
+    REQUIRE(!TrimmableKey<notrim_00>);
+    REQUIRE(!TrimmableKey<notrim_00 &>);
+    REQUIRE(!TrimmableKey<const notrim_00 &>);
+    REQUIRE(!TrimmableKey<notrim_00 &&>);
+
+    REQUIRE(TrimmableKey<ns::trim_00>);
+    REQUIRE(TrimmableKey<ns::trim_00 &>);
+    REQUIRE(TrimmableKey<const ns::trim_00 &>);
+    REQUIRE(TrimmableKey<ns::trim_00 &&>);
+
+    REQUIRE(!TrimmableKey<ns::trim_01>);
+    REQUIRE(TrimmableKey<ns::trim_01 &>);
+    REQUIRE(!TrimmableKey<const ns::trim_01 &>);
+    REQUIRE(!TrimmableKey<ns::trim_01 &&>);
+
+    REQUIRE(!TrimmableKey<ns::trim_02>);
+    REQUIRE(!TrimmableKey<ns::trim_02 &>);
+    REQUIRE(!TrimmableKey<ns::trim_02 &>);
+    REQUIRE(!TrimmableKey<ns::trim_02 &&>);
+
+    REQUIRE(!TrimmableKey<ns::trim_03>);
+    REQUIRE(!TrimmableKey<ns::trim_03 &>);
+    REQUIRE(!TrimmableKey<ns::trim_03 &>);
+    REQUIRE(!TrimmableKey<ns::trim_03 &&>);
+
+    REQUIRE(TrimmableKey<trim_ext>);
+    REQUIRE(TrimmableKey<trim_ext &>);
+    REQUIRE(TrimmableKey<const trim_ext &>);
+    REQUIRE(TrimmableKey<trim_ext &&>);
+
+    REQUIRE(!TrimmableKey<notrim_ext_00>);
+    REQUIRE(!TrimmableKey<notrim_ext_00 &>);
+    REQUIRE(!TrimmableKey<const notrim_ext_00 &>);
+    REQUIRE(!TrimmableKey<notrim_ext_00 &&>);
+
+    REQUIRE(!TrimmableKey<notrim_ext_01>);
+    REQUIRE(TrimmableKey<notrim_ext_01 &>);
+    REQUIRE(!TrimmableKey<const notrim_ext_01 &>);
+    REQUIRE(!TrimmableKey<notrim_ext_01 &&>);
+#endif
+}
