5dce0f244cac2623c56f5c898f9163053b41a747
Francesco Biscani <bluescarni@gmail.com>
2019-10-13 22:25:33 +0200

    s11n header + serialization for 128bit ints.

---
+++ include/obake/s11n.hpp
@@ -0,0 +1,79 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)::polynomials
+//
+// This file is part of the obake library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#ifndef OBAKE_S11N_HPP
+#define OBAKE_S11N_HPP
+
+#include <cstdint>
+
+#include <boost/config.hpp>
+#include <boost/mpl/equal_to.hpp>
+#include <boost/mpl/int.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/split_free.hpp>
+#include <boost/serialization/tracking.hpp>
+#include <boost/static_assert.hpp>
+
+#include <obake/config.hpp>
+
+#if defined(OBAKE_HAVE_GCC_INT128)
+
+namespace boost::serialization
+{
+
+template <class Archive>
+inline void save(Archive &ar, const __uint128_t &n, unsigned)
+{
+    // NOTE: save first the lower and then the upper limb.
+    ar << static_cast<::std::uint64_t>(n);
+    ar << static_cast<::std::uint64_t>(n >> 64);
+}
+
+template <class Archive>
+inline void load(Archive &ar, __uint128_t &n, unsigned)
+{
+    ::std::uint64_t tmp;
+
+    // Load the lower limb.
+    ar >> tmp;
+    n = tmp;
+
+    // Load the upper limb, add it.
+    ar >> tmp;
+    n += static_cast<__uint128_t>(tmp) << 64;
+}
+
+// For __int128_t, just delegate to the unsigned counterpart.
+template <class Archive>
+inline void save(Archive &ar, const __int128_t &n, unsigned)
+{
+    ar << static_cast<__uint128_t>(n);
+}
+
+template <class Archive>
+inline void load(Archive &ar, __int128_t &n, unsigned)
+{
+    __uint128_t tmp;
+    ar >> tmp;
+    n = static_cast<__int128_t>(tmp);
+}
+
+// Ensure that 128-bit integers are considered primitive types,
+// which also ensures that their address is never tracked.
+BOOST_STATIC_ASSERT((mpl::equal_to<implementation_level<__uint128_t>, mpl::int_<primitive_type>>::value));
+BOOST_STATIC_ASSERT((mpl::equal_to<implementation_level<__int128_t>, mpl::int_<primitive_type>>::value));
+
+} // namespace boost::serialization
+
+BOOST_SERIALIZATION_SPLIT_FREE(__uint128_t)
+BOOST_SERIALIZATION_SPLIT_FREE(__int128_t)
+
+#endif
+
+#endif
---
+++ test/s11n.cpp
@@ -0,0 +1,121 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the obake library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <sstream>
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+#include <obake/config.hpp>
+#include <obake/s11n.hpp>
+
+#include "catch.hpp"
+
+TEST_CASE("s11n_test")
+{
+#if defined(OBAKE_HAVE_GCC_INT128)
+    // Test serialisation for the 128-bit integral types.
+    {
+        std::stringstream ss;
+        __uint128_t out;
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << __uint128_t(0);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == 0u);
+        ss.str("");
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << __uint128_t(42);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == 42u);
+        ss.str("");
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << ((__uint128_t(42) << 64) + 42u);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == ((__uint128_t(42) << 64) + 42u));
+        ss.str("");
+    }
+
+    {
+        std::stringstream ss;
+        __int128_t out;
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << __int128_t(0);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == 0);
+        ss.str("");
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << __int128_t(42);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == 42);
+        ss.str("");
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << ((__int128_t(42) << 64) + 42);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == ((__int128_t(42) << 64) + 42));
+        ss.str("");
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << -__int128_t(42);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == -42);
+        ss.str("");
+
+        {
+            boost::archive::binary_oarchive oarchive(ss);
+            oarchive << -((__int128_t(42) << 64) + 42);
+        }
+        {
+            boost::archive::binary_iarchive iarchive(ss);
+            iarchive >> out;
+        }
+        REQUIRE(out == -((__int128_t(42) << 64) + 42));
+        ss.str("");
+    }
+#endif
+}
