d322a9cf0698dec2f969191e8db823188962e1ca
Francesco Biscani <bluescarni@gmail.com>
2019-10-15 14:31:37 +0200

    Ensure that the packing deltas are odd numbers.

--- include/obake/k_packing.hpp
+++ include/obake/k_packing.hpp
@@ -117,9 +117,15 @@ constexpr auto k_packing_compute_deltas()
 
             // The generated random number will have the top two bits
             // set to 1. This ensures that the generated deltas don't vary
-            // too much, while still retaining some randomicity in the lower bits.
+            // too much, while still retaining some randomness in the lower bits.
             const auto hi = T(3) << (cur_nbits - 2u);
-            const auto lo = static_cast<T>(rnd >> (static_cast<unsigned>(limits_digits<uint_t>) - cur_nbits + 2u));
+            // NOTE: ensure that the number will be odd. This helps avoiding
+            // power-of-2 commensurabilities when we use the packed value
+            // as a hash in a table with power-of-2 sizes.
+            // NOTE: perhaps it would be even better to look for a prime
+            // number, keep it in mind for the future.
+            const auto lo
+                = static_cast<T>((rnd >> (static_cast<unsigned>(limits_digits<uint_t>) - cur_nbits + 2u)) | uint_t(1));
 
             retval[i][j] = hi + lo;
         }
