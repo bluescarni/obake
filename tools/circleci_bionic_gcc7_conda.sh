106ae77d1ad4e5413f4942e725ed811368e967d9
Francesco Biscani <bluescarni@gmail.com>
2020-01-08 13:56:07 +0100

    Small cleanups in the CI scripts.

--- tools/circleci_bionic_gcc7_conda.sh
+++ tools/circleci_bionic_gcc7_conda.sh
@@ -11,18 +11,12 @@ sudo apt-get install build-essential
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 export deps_dir=$HOME/local
-export PATH="$HOME/miniconda/bin:$PATH"
 bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
 conda config --add channels conda-forge --force
-
 conda_pkgs="cmake>=3.3 mppp boost-cpp tbb tbb-devel abseil-cpp backtrace sphinx sphinx_rtd_theme"
-
 conda create -q -p $deps_dir -y $conda_pkgs
 source activate $deps_dir
-
-export deps_dir=$HOME/local
-export PATH="$HOME/miniconda/bin:$PATH"
-export PATH="$deps_dir/bin:$PATH"
 
 # Create the build dir and cd into it.
 mkdir build
