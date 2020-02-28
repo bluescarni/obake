552151409bb1063724821eab9ab7cc8d2b4d3c30
Francesco Biscani <bluescarni@gmail.com>
2020-02-28 16:56:37 +0100

    Tentative CircleCI fixes.

--- tools/circleci_focal_gcc9_coverage.sh
+++ tools/circleci_focal_gcc9_coverage.sh
@@ -24,7 +24,7 @@ make -j2 VERBOSE=1
 # Run the tests.
 ctest -V
 # Upload coverage data.
-bash <(curl -s https://codecov.io/bash) -x gcov-8 > /dev/null
+bash <(curl -s https://codecov.io/bash) -x gcov-9 > /dev/null
 
 set +e
 set +x
