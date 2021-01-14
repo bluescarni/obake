b153267dd5cd104fa2c4fbab11cb5e6420391fe0
Francesco Biscani <bluescarni@gmail.com>
2021-01-14 22:34:49 +0100

    More WIP, some initial testing.

--- include/obake/power_series/power_series.hpp
+++ include/obake/power_series/power_series.hpp
@@ -9,19 +9,26 @@
 #ifndef OBAKE_POWER_SERIES_POWER_SERIES_HPP
 #define OBAKE_POWER_SERIES_POWER_SERIES_HPP
 
+#include <array>
 #include <cstddef>
 #include <ostream>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <type_traits>
 #include <utility>
 #include <variant>
+#include <vector>
 
 #include <boost/flyweight/flyweight.hpp>
 #include <boost/flyweight/hashed_factory.hpp>
 
+#include <fmt/format.h>
+
 #include <obake/config.hpp>
 #include <obake/detail/fw_utils.hpp>
+#include <obake/detail/it_diff_check.hpp>
+#include <obake/detail/ss_func_forward.hpp>
 #include <obake/hash.hpp>
 #include <obake/math/degree.hpp>
 #include <obake/math/p_degree.hpp>
@@ -189,9 +196,7 @@ using psk_pdeg_t = detected_t<detail::key_p_degree_t, ::std::add_lvalue_referenc
 // in a variant, and for general copy/move
 // operations), and it also ensures
 // truncability via the polynomial implementation
-// (which we need to implement the truncate()
-// helper in power_series).
-// TODO do we have a truncate() helper?
+// (which we need to implement the truncate() function).
 template <typename K>
 using is_power_series_key
     = ::std::conjunction<is_key<K>,
@@ -224,12 +229,111 @@ template <typename K, typename C,
 #endif
 using p_series = series<K, C, power_series::tag<detail::psk_deg_t<K>>>;
 
+namespace detail
+{
+
+template <typename T>
+struct is_any_p_series_impl : ::std::false_type {
+};
+
+template <typename K, typename C>
+struct is_any_p_series_impl<p_series<K, C>> : ::std::true_type {
+};
+
+} // namespace detail
+
+// Detect power series.
+template <typename T>
+using is_any_p_series = detail::is_any_p_series_impl<T>;
+
+template <typename T>
+inline constexpr bool is_any_p_series_v = is_any_p_series<T>::value;
+
+#if defined(OBAKE_HAVE_CONCEPTS)
+
+template <typename T>
+OBAKE_CONCEPT_DECL any_p_series = is_any_p_series_v<T>;
+
+#endif
+
+namespace power_series
+{
+
+// Implementation of (partial) degree truncation for power series.
+template <
+    typename K, typename C, typename T,
+    ::std::enable_if_t<is_less_than_comparable_v<::std::add_lvalue_reference_t<const T>, ::obake::detail::psk_deg_t<K>>,
+                       int> = 0>
+inline void truncate_degree(p_series<K, C> &ps, const T &d)
+{
+    // Use the default functor for the extraction of the term degree.
+    // NOTE: d_impl is assured to work thanks to the concept
+    // requirements for ps key. The only extra bit we need in this function
+    // is to be able to compare d to the degree type, which
+    // is checked above.
+    using d_impl = customisation::internal::series_default_degree_impl;
+
+    // Implement on top of filter().
+    ::obake::filter(ps, [deg_ext = d_impl::d_extractor<p_series<K, C>>{&ps.get_symbol_set()}, &d](const auto &t) {
+        return !(d < deg_ext(t));
+    });
+}
+
+template <
+    typename K, typename C, typename T,
+    ::std::enable_if_t<is_less_than_comparable_v<::std::add_lvalue_reference_t<const T>, ::obake::detail::psk_deg_t<K>>,
+                       int> = 0>
+inline void truncate_p_degree(p_series<K, C> &ps, const T &d, const symbol_set &s)
+{
+    // Use the default functor for the extraction of the term degree.
+    // NOTE: d_impl is assured to work thanks to the concept
+    // requirements for ps key. The only extra bit we need in this function
+    // is to be able to compare d to the degree type, which
+    // is checked above.
+    using d_impl = customisation::internal::series_default_p_degree_impl;
+
+    // Extract the symbol indices.
+    const auto &ss = ps.get_symbol_set();
+    const auto si = ::obake::detail::ss_intersect_idx(s, ss);
+
+    // Implement on top of filter().
+    ::obake::filter(ps, [deg_ext = d_impl::d_extractor<p_series<K, C>>{&s, &si, &ss}, &d](const auto &t) {
+        return !(d < deg_ext(t));
+    });
+}
+
+} // namespace power_series
+
+// Set total degree truncation.
 template <
     typename K, typename C, typename T,
     ::std::enable_if_t<is_safely_castable_v<::std::add_lvalue_reference_t<const T>, detail::psk_deg_t<K>>, int> = 0>
 inline p_series<K, C> &set_truncation(p_series<K, C> &ps, const T &d)
 {
-    ps.tag().trunc = power_series::detail::trunc_t<detail::psk_deg_t<K>>(::obake::safe_cast<detail::psk_deg_t<K>>(d));
+    // Convert safely d to the degree type.
+    const auto deg = ::obake::safe_cast<detail::psk_deg_t<K>>(d);
+
+    try {
+        // Proceed with the truncation.
+        // NOTE: truncate_degree() is ensured to work because deg
+        // is the degree type, which is required to be less-than
+        // comparable in the ps requirements.
+        power_series::truncate_degree(ps, deg);
+
+        // Set the truncation level in ps.
+        ps.tag().trunc = power_series::detail::trunc_t<detail::psk_deg_t<K>>(deg);
+
+        // LCOV_EXCL_START
+    } catch (...) {
+        // NOTE: if anything goes wrong, make sure we clear
+        // up ps before rethrowing. This will clear up terms
+        // and symbol set, and will reset the truncation
+        // tag to its def-cted state (i.e., no truncation).
+        ps.clear();
+
+        throw;
+    }
+    // LCOV_EXCL_STOP
 
     return ps;
 }
@@ -239,8 +343,30 @@ template <
     ::std::enable_if_t<is_safely_castable_v<::std::add_lvalue_reference_t<const T>, detail::psk_deg_t<K>>, int> = 0>
 inline p_series<K, C> &set_truncation(p_series<K, C> &ps, const T &d, symbol_set ss)
 {
-    ps.tag().trunc = power_series::detail::trunc_t<detail::psk_deg_t<K>>(
-        ::std::pair{::obake::safe_cast<detail::psk_deg_t<K>>(d), ::std::move(ss)});
+    // Convert safely d to the degree type.
+    const auto deg = ::obake::safe_cast<detail::psk_deg_t<K>>(d);
+
+    try {
+        // Proceed with the truncation.
+        // NOTE: truncate_p_degree() is ensured to work because deg
+        // is the degree type, which is required to be less-than
+        // comparable in the ps requirements.
+        power_series::truncate_p_degree(ps, deg, ss);
+
+        // Set the truncation level in ps.
+        ps.tag().trunc = power_series::detail::trunc_t<detail::psk_deg_t<K>>(::std::pair{deg, ::std::move(ss)});
+
+        // LCOV_EXCL_START
+    } catch (...) {
+        // NOTE: if anything goes wrong, make sure we clear
+        // up ps before rethrowing. This will clear up terms
+        // and symbol set, and will reset the truncation
+        // tag to its def-cted state (i.e., no truncation).
+        ps.clear();
+
+        throw;
+    }
+    // LCOV_EXCL_STOP
 
     return ps;
 }
@@ -252,6 +378,138 @@ inline p_series<K, C> &unset_truncation(p_series<K, C> &ps)
 
     return ps;
 }
+
+template <typename K, typename C>
+inline const auto &get_truncation(const p_series<K, C> &ps)
+{
+    return ps.tag().trunc.get();
+}
+
+// Factory functions for power series.
+namespace detail
+{
+
+// Enabler for make_p_series():
+// - T must be a power series,
+// - std::string can be constructed from each input Args,
+// - ps key can be constructed from a const int * range,
+// - ps cf can be constructed from an integral literal.
+template <typename T, typename... Args>
+using make_p_series_supported
+    = ::std::conjunction<is_any_p_series<T>, ::std::is_constructible<::std::string, const Args &>...,
+                         ::std::is_constructible<series_key_t<T>, const int *, const int *>,
+                         ::std::is_constructible<series_cf_t<T>, int>>;
+
+template <typename T, typename... Args>
+using make_p_series_enabler = ::std::enable_if_t<make_p_series_supported<T, Args...>::value, int>;
+
+// Overload without a symbol set, no truncation.
+template <typename T, typename... Args, make_p_series_enabler<T, Args...> = 0>
+inline auto make_p_series_impl(const Args &...names)
+{
+    [[maybe_unused]] auto make_p_series = [](const auto &n) {
+        using str_t = remove_cvref_t<decltype(n)>;
+
+        // Init the retval, assign a symbol set containing only n.
+        T retval;
+        if constexpr (::std::is_same_v<str_t, ::std::string>) {
+            retval.set_symbol_set(symbol_set{n});
+        } else {
+            retval.set_symbol_set(symbol_set{::std::string(n)});
+        }
+
+        constexpr int arr[] = {1};
+
+        // Create and add a new term.
+        retval.add_term(series_key_t<T>(&arr[0], &arr[0] + 1), 1);
+
+        return retval;
+    };
+
+    return ::std::array<T, sizeof...(Args)>{{make_p_series(names)...}};
+}
+
+// Overload with a symbol set, no truncation.
+template <typename T, typename... Args, make_p_series_enabler<T, Args...> = 0>
+inline auto make_p_series_impl(const symbol_set &ss, const Args &...names)
+{
+    // Create a temp vector of ints which we will use to
+    // init the keys.
+    ::std::vector<int> tmp(::obake::safe_cast<::std::vector<int>::size_type>(ss.size()));
+
+    // Create the fw version of the symbol set.
+    const detail::ss_fw ss_fw(ss);
+
+    [[maybe_unused]] auto make_p_series = [&ss_fw, &ss, &tmp](const auto &n) {
+        using str_t = remove_cvref_t<decltype(n)>;
+
+        // Fetch a const reference to either the original
+        // std::string object n, or to a string temporary
+        // created from it.
+        const auto &s = [&n]() -> decltype(auto) {
+            if constexpr (::std::is_same_v<str_t, ::std::string>) {
+                return n;
+            } else {
+                return ::std::string(n);
+            }
+        }();
+
+        // Init the retval, assign the symbol set.
+        T retval;
+        retval.set_symbol_set_fw(ss_fw);
+
+        // Try to locate s within the symbol set.
+        const auto it = ss.find(s);
+        if (obake_unlikely(it == ss.end() || *it != s)) {
+            using namespace ::fmt::literals;
+
+            obake_throw(::std::invalid_argument,
+                        "Cannot create a power series with symbol set {} from the "
+                        "generator '{}': the generator is not in the symbol set"_format(detail::to_string(ss), s));
+        }
+
+        // Set to 1 the exponent of the corresponding generator.
+        tmp[static_cast<::std::vector<int>::size_type>(ss.index_of(it))] = 1;
+
+        // Create and add a new term.
+        // NOTE: at least for some monomial types (e.g., packed monomial),
+        // we will be computing the iterator difference when constructing from
+        // a range. Make sure we can safely represent the size of tmp via
+        // iterator difference.
+        ::obake::detail::it_diff_check<decltype(::std::as_const(tmp).data())>(tmp.size());
+        retval.add_term(series_key_t<T>(::std::as_const(tmp).data(), ::std::as_const(tmp).data() + tmp.size()), 1);
+
+        // Set back to zero the exponent that was previously set to 1.
+        tmp[static_cast<::std::vector<int>::size_type>(ss.index_of(it))] = 0;
+
+        return retval;
+    };
+
+    return ::std::array<T, sizeof...(Args)>{{make_p_series(names)...}};
+}
+
+} // namespace detail
+
+#if defined(OBAKE_MSVC_LAMBDA_WORKAROUND)
+
+template <typename T>
+struct make_p_series_msvc {
+    template <typename... Args>
+    constexpr auto operator()(const Args &...args) const
+        OBAKE_SS_FORWARD_MEMBER_FUNCTION(detail::make_p_series_impl<T>(args...))
+};
+
+template <typename T>
+inline constexpr auto make_p_series = make_p_series_msvc<T>{};
+
+#else
+
+// Power series creation functor.
+template <typename T>
+inline constexpr auto make_p_series
+    = [](const auto &...args) OBAKE_SS_FORWARD_LAMBDA(detail::make_p_series_impl<T>(args...));
+
+#endif
 
 } // namespace obake
 
--- test/power_series_00.cpp
+++ test/power_series_00.cpp
@@ -7,21 +7,115 @@
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include <cstdint>
+#include <initializer_list>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <variant>
 
+#include <obake/config.hpp>
+#include <obake/math/degree.hpp>
 #include <obake/polynomials/packed_monomial.hpp>
 #include <obake/power_series/power_series.hpp>
+#include <obake/symbols.hpp>
 
 #include "catch.hpp"
+#include "test_utils.hpp"
 
 using namespace obake;
 
 TEST_CASE("basic")
 {
-    p_series<packed_monomial<std::int32_t>, double> foo;
+    obake_test::disable_slow_stack_traces();
 
+    // Type traits/concepts.
+    using pm_t = packed_monomial<std::int32_t>;
+    using ps_t = p_series<pm_t, double>;
+
+    REQUIRE(is_power_series_cf_v<double>);
+    REQUIRE(!is_power_series_cf_v<const double>);
+    REQUIRE(!is_power_series_cf_v<const double &>);
+    REQUIRE(!is_power_series_cf_v<void>);
+
+#if defined(OBAKE_HAVE_CONCEPTS)
+    REQUIRE(power_series_cf<double>);
+    REQUIRE(!power_series_cf<const double>);
+    REQUIRE(!power_series_cf<const double &>);
+    REQUIRE(!power_series_cf<void>);
+#endif
+
+    REQUIRE(is_power_series_key_v<pm_t>);
+    REQUIRE(!is_power_series_key_v<const pm_t>);
+    REQUIRE(!is_power_series_key_v<const pm_t &>);
+    REQUIRE(!is_power_series_key_v<void>);
+
+#if defined(OBAKE_HAVE_CONCEPTS)
+    REQUIRE(power_series_key<pm_t>);
+    REQUIRE(!power_series_key<const pm_t>);
+    REQUIRE(!power_series_key<const pm_t &>);
+    REQUIRE(!power_series_key<void>);
+#endif
+
+    REQUIRE(is_any_p_series_v<ps_t>);
+    REQUIRE(!is_any_p_series_v<const ps_t>);
+    REQUIRE(!is_any_p_series_v<ps_t &>);
+    REQUIRE(!is_any_p_series_v<void>);
+
+#if defined(OBAKE_HAVE_CONCEPTS)
+    REQUIRE(any_p_series<ps_t>);
+    REQUIRE(!any_p_series<const ps_t>);
+    REQUIRE(!any_p_series<ps_t &>);
+    REQUIRE(!any_p_series<void>);
+#endif
+
+    // Default construction of the tag.
+    ps_t foo;
+    REQUIRE(foo.tag().trunc.get().index() == 0u);
+
+    // Tag state after truncation setting.
+    set_truncation(foo, 5);
+    REQUIRE(foo.tag().trunc.get().index() == 1u);
     set_truncation(foo, 5, {"x", "y", "z"});
-    std::cout << foo << '\n';
-
+    REQUIRE(foo.tag().trunc.get().index() == 2u);
     unset_truncation(foo);
-    std::cout << foo << '\n';
+    REQUIRE(foo.tag().trunc.get().index() == 0u);
+
+    // Truncation getter.
+    REQUIRE(get_truncation(foo).index() == 0u);
+
+    // Factory functions.
+    {
+        // No truncation, no ss.
+        auto [x, y] = make_p_series<ps_t>("x", std::string{"y"});
+
+        REQUIRE(x.size() == 1u);
+        REQUIRE(x.begin()->first == pm_t{1});
+        REQUIRE(x.begin()->second == 1.);
+        REQUIRE(x.get_symbol_set() == symbol_set{"x"});
+
+        REQUIRE(y.size() == 1u);
+        REQUIRE(y.begin()->first == pm_t{1});
+        REQUIRE(y.begin()->second == 1.);
+        REQUIRE(y.get_symbol_set() == symbol_set{"y"});
+    }
+
+    {
+        // No truncation, with ss.
+        auto [x, z] = make_p_series<ps_t>(symbol_set{"x", "y", "z"}, std::string{"x"}, "z");
+
+        REQUIRE(x.size() == 1u);
+        REQUIRE(x.begin()->first == pm_t{1, 0, 0});
+        REQUIRE(x.begin()->second == 1.);
+        REQUIRE(x.get_symbol_set() == symbol_set{"x", "y", "z"});
+
+        REQUIRE(z.size() == 1u);
+        REQUIRE(z.begin()->first == pm_t{0, 0, 1});
+        REQUIRE(z.begin()->second == 1.);
+        REQUIRE(z.get_symbol_set() == symbol_set{"x", "y", "z"});
+
+        OBAKE_REQUIRES_THROWS_CONTAINS((make_p_series<ps_t>(symbol_set{"x", "y", "z"}, std::string{"x"}, "a")),
+                                       std::invalid_argument,
+                                       "Cannot create a power series with symbol set {'x', 'y', 'z'} from the "
+                                       "generator 'a': the generator is not in the symbol set");
+    }
 }
